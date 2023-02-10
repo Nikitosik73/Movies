2001d98d860ce826d5f90c441d362fd459918da3
nikita <paramonov20@bk.ru>
2023-02-10 22:53:50 +0300

    create pojo object

---
+++ app/src/main/java/com/example/movies/pojo/Rating.java
@@ -0,0 +1,17 @@
+package com.example.movies.pojo;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Rating {
+
+    @SerializedName("kp")
+    private String ratingMovie;
+
+    public Rating(String ratingMovie) {
+        this.ratingMovie = ratingMovie;
+    }
+
+    public String getRatingMovie() {
+        return ratingMovie;
+    }
+}
