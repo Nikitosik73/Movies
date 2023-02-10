3c919a25f5843235c72b9624cbf10efc8590e874
nikita <paramonov20@bk.ru>
2023-02-11 00:02:10 +0300

    Query params

--- app/src/main/java/com/example/movies/api/ApiService.java
+++ app/src/main/java/com/example/movies/api/ApiService.java
@@ -6,9 +6,10 @@ import java.util.List;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiService {
 
-    @GET("movie?token=RVGRHT3-KS8MS22-MNRSHR5-ZBK39EJ&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&page=2&limit=40")
-    Single<MovieResponse> loadMovies();
+    @GET("movie?token=RVGRHT3-KS8MS22-MNRSHR5-ZBK39EJ&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=40")
+    Single<MovieResponse> loadMovies(@Query("page") int page);
 }
---
+++ app/src/main/java/com/example/movies/view/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.movies.view;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.movies.R;
+import com.example.movies.api.ApiFactory;
+import com.example.movies.pojo.Movie;
+import com.example.movies.pojo.MovieResponse;
+import com.example.movies.viewmodel.MainViewModel;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private MainViewModel mainViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+        mainViewModel.getMovies().observe(this, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(List<Movie> movies) {
+                Log.d(TAG, movies.toString());
+            }
+        });
+        mainViewModel.loadMovies();
+    }
+}
